bff53a9c6b585d4fc714d7ba82433f8efff06a7c
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-21 21:54:18 +0530

    ADDED: Test case to validate exception thrown when file is not a csv

--- src/main/java/com/bridgelabz/indianstatescensusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/indianstatescensusanalyser/StateCensusAnalyser.java
@@ -12,9 +12,11 @@ import java.util.stream.StreamSupport;
 
 public class StateCensusAnalyser {
 
-    public static int loadIndianStateCensusData(String filePath) throws IOException
-    {
-        Reader reader = Files.newBufferedReader(Paths.get(filePath));
+    public int loadIndianStateCensusData(String filePath) throws StateCensusAnalyserException {
+        try(
+                Reader reader = Files.newBufferedReader(Paths.get(filePath))
+        ){
+
         CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
         csvToBeanBuilder.withType(CSVStateCensus.class);
         csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
@@ -23,5 +25,19 @@ public class StateCensusAnalyser {
         Iterable<CSVStateCensus>  csvIterable = () -> censusCSVIterator;
         int numberOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
         return numberOfEntries;
+    } catch (IOException e) {
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusException.CENSUS_FILE_PROBLEM,
+                    "Incorrect File path");
+
+        } catch (RuntimeException e){
+            String[] s = filePath.split("[.]");
+            System.out.println(s[1]);
+            if(s[1]!="csv"){
+                throw new StateCensusAnalyserException(StateCensusAnalyserException.CensusException.INCORRECT_TYPE_ISSUE,
+                        "Incorrect File extension");
+            }
+
+		}
+        return 0;
     }
 }
--- src/test/java/com/bridgelabz/indianstatescensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/indianstatescensusanalyser/StateCensusAnalyserTest.java
@@ -9,18 +9,21 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 
 public class StateCensusAnalyserTest {
+    public static final String CSV_FILE_WITHOUT_HEADER = "/home/rajatsp/bridgelabz/day29/assignment/IndianStatesCensusAnalyser/IndianStateCensusDataWithoutHeader.csv";
+    public static final String INCORRECT_CSV_FILE = "/home/rajatsp/bridgelabz/day29/assignment/IndianStatesCensusAnalyser/IndianStateCensusDataWithWrongDelimiter.csv";
+    public static final String NOT_A_CSV_FILE = "/home/rajatsp/bridgelabz/day29/assignment/IndianStatesCensusAnalyser/new.txt";
     public static final String FILE_NOT_EXIST = "incorrectFilePath.csv";
     public static final String CORRECT_CSV_FILE = "/home/rajatsp/bridgelabz/day29/assignment/IndianStatesCensusAnalyser/IndianStateCensusData.csv";
 
     @Test
-    public void givenIndianCensusCSVFile_ShouldReturnCorrectRecords() throws IOException, StateCensusAnalyserException {
+    public void givenIndianCensusCSVFile_ShouldReturnCorrectRecords() throws StateCensusAnalyserException {
         StateCensusAnalyser analyser = new StateCensusAnalyser();
         int count = analyser.loadIndianStateCensusData(CORRECT_CSV_FILE);
         assertEquals(29, count);
     }
 
     @Test
-    public void givenCSVFile_ReturnException_ifFileNotPresent() throws IOException {
+    public void givenCSVFile_ReturnException_ifFileNotPresent() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
             censusAnalyser.loadIndianStateCensusData(FILE_NOT_EXIST);
@@ -29,5 +32,13 @@ public class StateCensusAnalyserTest {
         }
     }
 
-
+    @Test
+    public void givenIncorrectCSVFile_ThrowExceptionForIncorrectCSVFile() {
+        try {
+            StateCensusAnalyser stateCensusAnalyserMain = new StateCensusAnalyser();
+            stateCensusAnalyserMain.loadIndianStateCensusData(NOT_A_CSV_FILE);
+        } catch (StateCensusAnalyserException e) {
+            assertEquals(StateCensusAnalyserException.CensusException.INCORRECT_TYPE_ISSUE, e.exceptionType);
+        }
+    }
 }
