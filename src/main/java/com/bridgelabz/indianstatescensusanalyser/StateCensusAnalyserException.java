89bf907a4a8a15108b44ef91b71a014510164b13
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-21 20:41:33 +0530

    ADDED: Exception class to throw exception when CSV file is invalid

---
+++ src/main/java/com/bridgelabz/indianstatescensusanalyser/StateCensusAnalyserException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.indianstatescensusanalyser;
+
+public class StateCensusAnalyserException extends Exception {
+    enum CensusException {
+        CENSUS_FILE_PROBLEM, INCORRECT_TYPE_ISSUE, DELIMITER_ISSUE, INCORRECT_HEADER_PROBLEM
+    }
+
+    CensusException exceptionType ;
+
+    public StateCensusAnalyserException(CensusException exceptionType, String message) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+}
